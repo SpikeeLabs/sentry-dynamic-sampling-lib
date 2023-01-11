2c06f0405658dc9efc2184a77b379b8084ad65f3
jeanloup.monnier <jean-loup.monnier@spikeelabs.fr>
2023-01-11 09:39:51 +0100

    feat: metric controller
    metric can be enable or disable by controller

--- sentry_dynamic_sampling_lib/shared.py
+++ sentry_dynamic_sampling_lib/shared.py
@@ -47,6 +47,12 @@ class Config:
     def ignored_tasks(self, new_ignored_tasks):
         self._ignored_tasks = set(new_ignored_tasks)
 
+    @synchronized
+    def update(self, data):
+        self._sample_rate = data["active_sample_rate"]
+        self._ignored_paths = data["wsgi_ignore_path"]
+        self._ignored_tasks = data["celery_ignore_task"]
+
 
 class MetricType(Enum):
     WSGI = "WSGI"
@@ -56,18 +62,30 @@ class MetricType(Enum):
 class Metric:
     def __init__(self) -> None:
         self._lock = RLock()
+        self._activate = {
+            MetricType.WSGI: False,
+            MetricType.CELERY: False,
+        }
         self._counters = {
             MetricType.WSGI: Counter(),
             MetricType.CELERY: Counter(),
         }
 
+    def set_mode(self, _type, mode):
+        self._activate[_type] = mode
+
+    def get_mode(self, _type):
+        return self._activate[_type]
+
     @synchronized
     def count_path(self, path):
-        self._counters[MetricType.WSGI][path] += 1
+        if self._activate[MetricType.WSGI]:
+            self._counters[MetricType.WSGI][path] += 1
 
     @synchronized
     def count_task(self, path):
-        self._counters[MetricType.CELERY][path] += 1
+        if self._activate[MetricType.CELERY]:
+            self._counters[MetricType.CELERY][path] += 1
 
     @synchronized
     def get_and_reset(self, _type):
