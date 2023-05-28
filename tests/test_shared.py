925c4a9314fbc0480fef84747969c89800c26add
jeanloup.monnier <jean-loup.monnier@spikeelabs.fr>
2023-05-28 16:42:05 +0200

    fix: unit test

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -29,27 +29,32 @@ def test_config():
     c.ignored_paths = ["a", "b"]
     assert c.ignored_paths == {"a", "b"}
 
-    # assert use lock
     assert synchronized_mock.call_count == 7
+
+    c.ignored_user_agents = ["a", "b"]
+    assert c.ignored_user_agents == ("a", "b")
+
+    # assert use lock
+    assert synchronized_mock.call_count == 9
 
     c.ignored_tasks = ["a", "b"]
     assert c.ignored_tasks == {"a", "b"}
 
     # assert use lock
-    assert synchronized_mock.call_count == 9
+    assert synchronized_mock.call_count == 11
 
     data = {"active_sample_rate": 10, "wsgi_ignore_path": (1, 2), "celery_ignore_task": (3, 4)}
     c.update(data)
 
     # assert use lock
-    assert synchronized_mock.call_count == 10
+    assert synchronized_mock.call_count == 12
 
     assert c.sample_rate == 10
     assert c.ignored_paths == {1, 2}
     assert c.ignored_tasks == {3, 4}
 
     # assert use lock
-    assert synchronized_mock.call_count == 13
+    assert synchronized_mock.call_count == 15
 
 
 def test_metric():
@@ -73,26 +78,33 @@ def test_metric():
     m.count_path("/metric/")
     assert synchronized_mock.call_count == 1
 
-    m.count_task("celery.run")
+    m.count_user_agent("kube/1.26")
     assert synchronized_mock.call_count == 2
 
-    # iteration
-    assert list(m) == [(MetricType.WSGI, Counter(["/metric/"])), (MetricType.CELERY, Counter(["celery.run"]))]
+    m.count_task("celery.run")
+    assert synchronized_mock.call_count == 3
 
-    assert synchronized_mock.call_count == 4
+    # iteration
+    assert list(m) == [
+        (MetricType.WSGI, {"path": Counter(["/metric/"]), "user_agent": Counter(["kube/1.26"])}),
+        (MetricType.CELERY, {"task": Counter(["celery.run"])}),
+    ]
+
+    assert synchronized_mock.call_count == 5
 
     assert list(m) == []
 
-    assert synchronized_mock.call_count == 6
+    assert synchronized_mock.call_count == 7
 
     m.set_mode(MetricType.WSGI, False)
     m.set_mode(MetricType.CELERY, False)
 
     assert list(m) == []
-    assert synchronized_mock.call_count == 6
+    assert synchronized_mock.call_count == 7
 
     m.count_path("/metric/")
+    m.count_user_agent("kube/1.26")
     m.count_task("celery.run")
 
-    assert synchronized_mock.call_count == 8
+    assert synchronized_mock.call_count == 10
     assert list(m) == []
